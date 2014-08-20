3e3e9bf207cd6477a6fa958cd534af4fa468f763
dev-vader <vincejweberiv@gmail.com>
2014-08-20 01:20:38 -0700

    Minor tweaks, will get more attention next update

--- src/com/chatserver/Server.java
+++ src/com/chatserver/Server.java
@@ -6,38 +6,55 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
-public class Server {
+class Server {
 
 	private final static int PORT = 40123;
 	private final static int MAX_PACKET_SIZE = 65507;
-	private static boolean connected = true;
+	private static boolean running = true;
 	
 	public static void main(String[] args) throws IOException{
 		
 		DatagramSocket socket = new DatagramSocket(PORT);
 
 		byte[] buffer = new byte[MAX_PACKET_SIZE];
+		byte[] jsonBuffer = new byte[MAX_PACKET_SIZE];
 		
 		DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
 		
 		System.out.println("Server up " + System.currentTimeMillis());
-		while(connected){
+		
+			while(running){
 			
-			socket.receive(inPacket);
+				socket.receive(inPacket);
 			
-			String receivedMessage = new String(inPacket.getData(), 0 , inPacket.getLength()) + " From Address: " +
-			inPacket.getAddress() + " , PORT: " + inPacket.getPort();
+				String receivedMessage = new String(inPacket.getData(), 0 , inPacket.getLength()) + " From Address: " +
+						inPacket.getAddress() + " , PORT: " + inPacket.getPort();
 			
-			System.out.println(receivedMessage);
+				jsonBuffer = inPacket.getData();
+				String jsonString = new String(jsonBuffer);
 			
-			BufferedReader writeMessage = new BufferedReader(new InputStreamReader(System.in));
-			String handledMessage = writeMessage.readLine();
-			buffer = handledMessage.getBytes();
-	
+				System.out.println(jsonString);
+				System.out.println(receivedMessage);
 			
-			DatagramPacket outPacket = new DatagramPacket(buffer, buffer.length, inPacket.getAddress() , 
-					inPacket.getPort());
-			socket.send(outPacket);
-		}
+				BufferedReader writeMessage = new BufferedReader(new InputStreamReader(System.in));
+				String messageContents = writeMessage.readLine();
+			
+					if (messageContents.equals("close")) {
+						running = false;
+					}
+			
+				buffer = messageContents.getBytes();
+			
+			
+				DatagramPacket outPacket = new DatagramPacket(buffer, buffer.length, inPacket.getAddress() , 
+							inPacket.getPort());
+			
+				socket.send(outPacket);
+			
+			} 
+		
+			socket.close();
+			System.out.println("SOCKET CLOSED");
+		
 	}
 }
